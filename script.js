7baebbcd1357c7d962807d91497f07a0c37c895c
Tumsifu Lema <tom455976@gmail.com>
2024-02-14 17:49:55 +0300

    restarted the pelson website again

---
+++ script.js
@@ -0,0 +1,8 @@
+const sidebar = document.querySelector(".sidebar");
+
+function openSideBar() {
+  sidebar.style.display = "flex";
+}
+function closeSideBar() {
+  sidebar.style.display = "none";
+}
