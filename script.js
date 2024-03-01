2694c4bc89368f467659db13aef44a72d632f1fb
Tumsifu Lema <tom455976@gmail.com>
2024-03-01 20:57:10 +0300

    added scrolling functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/**toggle sidebar functionality */
 const sidebar = document.querySelector(".sidebar");
 
 function openSideBar() {
@@ -5,4 +6,21 @@ function openSideBar() {
 }
 function closeSideBar() {
   sidebar.style.display = "none";
+}
+
+/**Visibilty for up arrow upon scrolling */
+window.onscroll = function () {
+  showOnScroll();
+};
+
+function showOnScroll() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    document.getElementById("up-arrow").style.display = "flex";
+  } else {
+    document.getElementById("up-arrow").style.display = "none";
+  }
+}
+/**The scroll to top functionality */
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
